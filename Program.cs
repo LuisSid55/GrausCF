fcf6d4d6645ddbad8e9220930ae1fc9106b9af1d
LuisSid55 <luiscitty4@gmail.com>
2021-09-26 01:34:02 -0300

    Pequena mudança

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace GrausCF
             Console.Write("\n°C: ");
             c = Convert.ToDouble(Console.ReadLine());
 
-            f = (c * 9 / 5) + 32;
+            f = (c * 1.8) + 32;
             
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine($"\n {c}°C equivale a {f}°F\n");
