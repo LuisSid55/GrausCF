a34594a488ebbffa191dcb9a0764931e4326b5bf
LuisSid55 <luiscitty4@gmail.com>
2021-09-25 21:06:56 -0300

    GrausCF

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f;
+            Console.WriteLine("\n--Conversor de °C para °F--");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("\n°C: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            f = (c * 9 / 5) + 32;
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\n {c}°C equivale a {f}°F\n");
+            Console.ResetColor();
+        }
+    }
+}
